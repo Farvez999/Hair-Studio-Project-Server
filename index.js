02a9d3ac92f66d3ae9fd9e979fa287b8dc577a3c
Farvez999 <engrfrzabdullah999@gmail.com>
2022-11-09 03:57:44 +0600

    Service single  Data

--- index.js
+++ index.js
@@ -31,6 +31,13 @@ async function run() {
             res.send(services)
         })
 
+        //single service
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const service = await serviceCollection.findOne(query)
+            res.send(service)
+        })
 
 
 
