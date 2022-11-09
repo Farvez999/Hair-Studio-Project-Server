c9893adc5801c275121c0211b5c05e4970d462e0
Farvez999 <engrfrzabdullah999@gmail.com>
2022-11-10 03:09:44 +0600

    Delete api added

--- index.js
+++ index.js
@@ -15,20 +15,20 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mordayw.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-// function verifyJWT(req, res, next) {
-//     const authHeader = req.headers.authorization;
-//     if (!authHeader) {
-//         return res.status(401).send({ message: 'unauthorized access' })
-//     }
-//     const token = authHeader.split(' ')[1];
-//     jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
-//         if (err) {
-//             return res.status(401).send({ message: 'unauthorized access' })
-//         }
-//         req.decoded = decoded;
-//         next();
-//     })
-// }
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 async function run() {
     try {
@@ -83,22 +83,68 @@ async function run() {
             res.send(result);
         })
 
+
         //all data reviews
+        // app.get('/reviews', async (req, res) => {
+
+        //     //JWT  Token
+        //     // const decoded = req.decoded;
+        //     // if (decoded.email !== req.query.email) {
+        //     //     res.status(403).send({ message: 'unauthorized access' })
+        //     // }
+
+        //     let query = {}
+        //     if (req.query.email) {
+        //         query = {
+        //             email: req.query.email
+        //         }
+        //     }
+        //     const cursor = reviewsCollection.find(query)
+        //     const review = await cursor.toArray()
+        //     res.send(review)
+        // })
+
+
         app.get('/reviews', async (req, res) => {
-            console.log(req.headers)
-            const query = {};
-            const cursor = reviewsCollection.find(query)
-            const reviews = await cursor.toArray()
-            res.send(reviews)
+            //JWT  Token
+            // const decoded = req.decoded;
+            // if (decoded.email !== req.query.email) {
+            //     res.status(403).send({ message: 'unauthorized access' })
+            // }
+
+            let query = {}
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            if (req.query.serviceId) {
+                query = {
+                    serviceId: req.query.serviceId
+                }
+            }
+            const cursor = reviewsCollection.find(query).sort({ _id: -1 })
+            const allReview = await cursor.toArray()
+            res.send(allReview)
         })
 
         //single review data
+
         app.get('/reviews/:id', async (req, res) => {
-            const query = req.params.id
-            console.log(query)
-            const cursor = reviewsCollection.find({ service: query }).sort({ _id: -1 })
-            const reviews = await cursor.toArray()
-            res.send(reviews)
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const review = await reviewsCollection.findOne(query)
+            res.send(review)
+        })
+
+        //delete review
+
+        app.delete('/delete/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewsCollection.deleteOne(query);
+            console.log(result)
+            res.send(result);
         })
 
     } finally {
